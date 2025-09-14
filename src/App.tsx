6187ef0e5d84cefa2d1851dd27332aff36222e60
Jennifer <thejenniferhaggerty@gmail.com>
2025-09-13 23:36:46 -0400

    Everything is setup and styled. May need to tweak the stylesheet when everything comes in but it's lookin' funky and cool!

--- src/App.tsx
+++ src/App.tsx
@@ -14,30 +14,20 @@ const App = () => {
     <main>
       <section>
         <h1>Style Guide</h1>
-        <div className="flex justify-around">
+        <div className="flex flex-col md:flex-row justify-around">
           <div>
             <div>
               <h2>Typography</h2>
             </div>
-            <div>
+            <div className="">
               <div className="h1">Headline h1</div>
               <div className="h2">Headline h2</div>
               <div className="h3">Headline h3</div>
               <div className="h4">Headline h4</div>
               <div className="h5">h5</div>
               <div className="h6">Headline h6</div>
-              <div>
-                <a href="#">Link</a>
-              </div>
-              <p>
-                Paragraph with a <a href="#">Link</a>.
-              </p>
-              <button onClick={() => setCount((count) => count + 1)}>
-                button click count is {count}
-              </button>
             </div>
           </div>
-
           <div>
             <div>
               <h2>Typography --Reference</h2>
@@ -53,7 +43,24 @@ const App = () => {
               </p>
               <div className="h4">Nights</div>
             </div>
+            <img
+              className="flex"
+              src="./src/assets/images/hhn24.jpg"
+              alt="the universal halloween horror nights 2024 green flannel shirt"
+            />
           </div>
+        </div>
+        <div>
+          <h3>Extras</h3>
+          <div>
+            <a href="#">Link</a>
+          </div>
+          <p>
+            Paragraph with a <a href="#">Link</a>.
+          </p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            button click count is {count}
+          </button>
         </div>
       </section>
       <section>
@@ -148,9 +155,7 @@ const App = () => {
                     <h4>Interface Developer at CQL Corp</h4>
                     <h5>1'23-12'24</h5>
                   </div>
-                  <div>
-                    <h6>リモート</h6>
-                  </div>
+                  <h6>remote</h6>
                 </div>
                 <p>
                   <ul>
