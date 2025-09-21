35a23b02e6d16039fd042bad4a91dfeedf74dc05
Jennifer <thejenniferhaggerty@gmail.com>
2025-09-21 09:41:31 -0400

    All set, just needs the inky background for pizazz

--- src/App.tsx
+++ src/App.tsx
@@ -31,18 +31,21 @@ const App = () => {
                 rel={item.rel}
               >
                 <img
-                  className="card-image"
+                  className="bw-image"
                   src={item["image-src"]}
                   alt={item["image-alt"]}
                 />
-                <div className="section">
-                  <div className="h5 badge">{item.tagline}</div>
+                <div className="section card-content">
+                  <div className="card-title h1">{item.title}</div>
+                  <div className="card-tagline h2">{item.tagline}</div>
+                  <div className="card-badge h5">{item.date}</div>
                   <img
                     className="card-image"
                     src={item["image-src"]}
                     alt={item["image-alt"]}
+                    aria-hidden="true"
                   />
-                  <div className="h4">{item.title}</div>
+                  <div className="h4">{item.location}</div>
                 </div>
               </a>
             </li>
