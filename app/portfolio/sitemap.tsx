25b915e18bae306d3198c8d2d71efe81518408e7
Jennifer <thejenniferhaggerty@gmail.com>
2026-02-08 16:34:27 -0500

    Needed to specify host for portfolio sitemap

--- app/portfolio/sitemap.tsx
+++ app/portfolio/sitemap.tsx
@@ -4,7 +4,7 @@ import { getSlugFromTitle } from "../../src/function";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return site.portfolio.map((item) => ({
-    url: `/portfolio/${getSlugFromTitle(item.title)}`,
+    url: `${process.env["HOST"]}/portfolio/${getSlugFromTitle(item.title)}`,
     lastModified: new Date(),
   }));
 }
