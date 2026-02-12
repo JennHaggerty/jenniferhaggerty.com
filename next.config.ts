43297d6cd981b9166fad9c9f649af949bfd2aa06
Jennifer <thejenniferhaggerty@gmail.com>
2026-02-12 16:49:31 -0500

    Added permanent redirects for google indexing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,65 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "http://jenniferhaggerty.com/",
+        destination: "https://www.jenniferhaggerty.com/",
+        permanent: true,
+      },
+      {
+        source: "https://jenniferhaggerty.com/",
+        destination: "https://www.jenniferhaggerty.com/",
+        permanent: true,
+      },
+      {
+        source: "http://www.jenniferhaggerty.com/",
+        destination: "https://www.jenniferhaggerty.com/",
+        permanent: true,
+      },
+      {
+        source: "/about/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/jennifer_haggerty_interface_developer.pdf",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/jhaggerty_resume.pdf",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/making-a-great-image-lighting/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/gallery-category/photography/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/project/anastasiya/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/project/melanie-beauty/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/project/sith-egan/",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
